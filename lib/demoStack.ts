8870339bfa2ba6af7d49184173d0fe28fa400e34
sunil jangid <matt348806@gmail.com>
2025-01-07 13:59:40 +0530

    fixed s3 bucket name issue

--- lib/demoStack.ts
+++ lib/demoStack.ts
@@ -29,7 +29,7 @@ export class DemoAppStack extends cdk.Stack {
 
     const level2S3Bucket = new Bucket(this, "demo-storage-bucket", {
       versioned: true,
-      bucketName: `demo-bucket-348806-unique`,
+      bucketName: `mybucket-${stageName}`,
       removalPolicy: removalPolicy,
       autoDeleteObjects: removalPolicy === cdk.RemovalPolicy.DESTROY,
     });
